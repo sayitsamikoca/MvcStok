ae191b6eb25f75f84f13ccb52e55e8b8e981df60
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-04-18 00:43:41 +0300

    Kategori Listeleme Yapıldı.

--- MvcStok/Controllers/KategoriController.cs
+++ MvcStok/Controllers/KategoriController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcStok.Models.Entity; // Models altındaki entity klasörüne ulaşbilmek için, kütüphane olarak tanımladım.
 
 namespace MvcStok.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        MvcDbStokEntities dbStokEntities = new MvcDbStokEntities();
         public ActionResult Index()
         {
-            return View();
+            var kategoriler = dbStokEntities.TBLKATEGORILER.ToList(); // Kategoriler listesindeki değerleri listele
+
+            return View(kategoriler); // bana kategorileri döndür.
+            
         }
     }
 }
