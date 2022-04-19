1935b6a69ac9667096412a5c3c21a9bcfd2d804c
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-04-20 01:45:15 +0300

    Ürün Ekleme için kategoriler dropdown listeden çekilme, yazıldı

--- MvcStok/Controllers/UrunController.cs
+++ MvcStok/Controllers/UrunController.cs
@@ -20,6 +20,16 @@ namespace MvcStok.Controllers
         [HttpGet]
         public ActionResult UrunEkle()
         {
+            // LINQ İle sorgulama yaptık - DropdownList için db'den veri seçme [Ürün ekleme yaparken kullanacağız.]
+            List<SelectListItem> degerler = (from i in dbStokEntities.TBLKATEGORILER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+                                             }).ToList();
+
+            ViewBag.dgr = dbStokEntities; // Controller tarafındaki ifadeyi diğer tarafa taşıyacağız - nesne türetip orada kullanacağız.
+
             return View();
         }
 
