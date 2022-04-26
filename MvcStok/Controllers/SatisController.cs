82a54079938c41771edbdfcc2f46c2d1605aba91
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-04-26 23:53:55 +0300

    Popup içinde Satış İşlemi Yazıldı, Test Başarılı

--- MvcStok/Controllers/SatisController.cs
+++ MvcStok/Controllers/SatisController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcStok.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,26 @@ namespace MvcStok.Controllers
     public class SatisController : Controller
     {
         // GET: Satis
+        MvcDbStokEntities dbStokEntities = new MvcDbStokEntities();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR satis)
+        {
+            dbStokEntities.TBLSATISLAR.Add(satis);
+            dbStokEntities.SaveChanges();
+            return View("Index");
         }
     }
 }
